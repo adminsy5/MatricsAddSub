1ffae7f8dbab68aa15006eaa8ec228a8afb622d3
adminsy5 <piyush@cbigujarat.onmicrosoft.com>
2023-01-22 07:35:08 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.a1 = New System.Windows.Forms.TextBox()
+        Me.a2 = New System.Windows.Forms.TextBox()
+        Me.a4 = New System.Windows.Forms.TextBox()
+        Me.a3 = New System.Windows.Forms.TextBox()
+        Me.b4 = New System.Windows.Forms.TextBox()
+        Me.b3 = New System.Windows.Forms.TextBox()
+        Me.b2 = New System.Windows.Forms.TextBox()
+        Me.b1 = New System.Windows.Forms.TextBox()
+        Me.suml = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.sa1 = New System.Windows.Forms.Label()
+        Me.sa2 = New System.Windows.Forms.Label()
+        Me.sa4 = New System.Windows.Forms.Label()
+        Me.sa3 = New System.Windows.Forms.Label()
+        Me.sb4 = New System.Windows.Forms.Label()
+        Me.sb3 = New System.Windows.Forms.Label()
+        Me.sb2 = New System.Windows.Forms.Label()
+        Me.sb1 = New System.Windows.Forms.Label()
+        Me.calculate = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(199, 77)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(140, 45)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Matrix A"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(560, 77)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(137, 45)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Matrix B"
+        '
+        'a1
+        '
+        Me.a1.Location = New System.Drawing.Point(132, 141)
+        Me.a1.Multiline = True
+        Me.a1.Name = "a1"
+        Me.a1.Size = New System.Drawing.Size(117, 59)
+        Me.a1.TabIndex = 2
+        '
+        'a2
+        '
+        Me.a2.Location = New System.Drawing.Point(282, 141)
+        Me.a2.Multiline = True
+        Me.a2.Name = "a2"
+        Me.a2.Size = New System.Drawing.Size(117, 59)
+        Me.a2.TabIndex = 3
+        '
+        'a4
+        '
+        Me.a4.Location = New System.Drawing.Point(282, 224)
+        Me.a4.Multiline = True
+        Me.a4.Name = "a4"
+        Me.a4.Size = New System.Drawing.Size(117, 59)
+        Me.a4.TabIndex = 5
+        '
+        'a3
+        '
+        Me.a3.Location = New System.Drawing.Point(132, 224)
+        Me.a3.Multiline = True
+        Me.a3.Name = "a3"
+        Me.a3.Size = New System.Drawing.Size(117, 59)
+        Me.a3.TabIndex = 4
+        '
+        'b4
+        '
+        Me.b4.Location = New System.Drawing.Point(646, 224)
+        Me.b4.Multiline = True
+        Me.b4.Name = "b4"
+        Me.b4.Size = New System.Drawing.Size(117, 59)
+        Me.b4.TabIndex = 9
+        '
+        'b3
+        '
+        Me.b3.Location = New System.Drawing.Point(496, 224)
+        Me.b3.Multiline = True
+        Me.b3.Name = "b3"
+        Me.b3.Size = New System.Drawing.Size(117, 59)
+        Me.b3.TabIndex = 8
+        '
+        'b2
+        '
+        Me.b2.Location = New System.Drawing.Point(646, 141)
+        Me.b2.Multiline = True
+        Me.b2.Name = "b2"
+        Me.b2.Size = New System.Drawing.Size(117, 59)
+        Me.b2.TabIndex = 7
+        '
+        'b1
+        '
+        Me.b1.Location = New System.Drawing.Point(496, 141)
+        Me.b1.Multiline = True
+        Me.b1.Name = "b1"
+        Me.b1.Size = New System.Drawing.Size(117, 59)
+        Me.b1.TabIndex = 6
+        '
+        'suml
+        '
+        Me.suml.AutoSize = True
+        Me.suml.Location = New System.Drawing.Point(132, 457)
+        Me.suml.Name = "suml"
+        Me.suml.Size = New System.Drawing.Size(225, 32)
+        Me.suml.TabIndex = 10
+        Me.suml.Text = "Sum of Two Matrics"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(496, 457)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(294, 32)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Subtraction of two matrics"
+        '
+        'sa1
+        '
+        Me.sa1.AutoSize = True
+        Me.sa1.Location = New System.Drawing.Point(148, 516)
+        Me.sa1.Name = "sa1"
+        Me.sa1.Size = New System.Drawing.Size(0, 32)
+        Me.sa1.TabIndex = 12
+        '
+        'sa2
+        '
+        Me.sa2.AutoSize = True
+        Me.sa2.Location = New System.Drawing.Point(261, 516)
+        Me.sa2.Name = "sa2"
+        Me.sa2.Size = New System.Drawing.Size(0, 32)
+        Me.sa2.TabIndex = 13
+        '
+        'sa4
+        '
+        Me.sa4.AutoSize = True
+        Me.sa4.Location = New System.Drawing.Point(261, 571)
+        Me.sa4.Name = "sa4"
+        Me.sa4.Size = New System.Drawing.Size(0, 32)
+        Me.sa4.TabIndex = 15
+        '
+        'sa3
+        '
+        Me.sa3.AutoSize = True
+        Me.sa3.Location = New System.Drawing.Point(148, 571)
+        Me.sa3.Name = "sa3"
+        Me.sa3.Size = New System.Drawing.Size(0, 32)
+        Me.sa3.TabIndex = 14
+        '
+        'sb4
+        '
+        Me.sb4.AutoSize = True
+        Me.sb4.Location = New System.Drawing.Point(630, 571)
+        Me.sb4.Name = "sb4"
+        Me.sb4.Size = New System.Drawing.Size(0, 32)
+        Me.sb4.TabIndex = 19
+        '
+        'sb3
+        '
+        Me.sb3.AutoSize = True
+        Me.sb3.Location = New System.Drawing.Point(517, 571)
+        Me.sb3.Name = "sb3"
+        Me.sb3.Size = New System.Drawing.Size(0, 32)
+        Me.sb3.TabIndex = 18
+        '
+        'sb2
+        '
+        Me.sb2.AutoSize = True
+        Me.sb2.Location = New System.Drawing.Point(630, 516)
+        Me.sb2.Name = "sb2"
+        Me.sb2.Size = New System.Drawing.Size(0, 32)
+        Me.sb2.TabIndex = 17
+        '
+        'sb1
+        '
+        Me.sb1.AutoSize = True
+        Me.sb1.Location = New System.Drawing.Point(517, 516)
+        Me.sb1.Name = "sb1"
+        Me.sb1.Size = New System.Drawing.Size(0, 32)
+        Me.sb1.TabIndex = 16
+        '
+        'calculate
+        '
+        Me.calculate.Location = New System.Drawing.Point(366, 679)
+        Me.calculate.Name = "calculate"
+        Me.calculate.Size = New System.Drawing.Size(150, 46)
+        Me.calculate.TabIndex = 20
+        Me.calculate.Text = "Calculate"
+        Me.calculate.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 32.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(923, 787)
+        Me.Controls.Add(Me.calculate)
+        Me.Controls.Add(Me.sb4)
+        Me.Controls.Add(Me.sb3)
+        Me.Controls.Add(Me.sb2)
+        Me.Controls.Add(Me.sb1)
+        Me.Controls.Add(Me.sa4)
+        Me.Controls.Add(Me.sa3)
+        Me.Controls.Add(Me.sa2)
+        Me.Controls.Add(Me.sa1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.suml)
+        Me.Controls.Add(Me.b4)
+        Me.Controls.Add(Me.b3)
+        Me.Controls.Add(Me.b2)
+        Me.Controls.Add(Me.b1)
+        Me.Controls.Add(Me.a4)
+        Me.Controls.Add(Me.a3)
+        Me.Controls.Add(Me.a2)
+        Me.Controls.Add(Me.a1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents a1 As TextBox
+    Friend WithEvents a2 As TextBox
+    Friend WithEvents a4 As TextBox
+    Friend WithEvents a3 As TextBox
+    Friend WithEvents b4 As TextBox
+    Friend WithEvents b3 As TextBox
+    Friend WithEvents b2 As TextBox
+    Friend WithEvents b1 As TextBox
+    Friend WithEvents suml As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents sa1 As Label
+    Friend WithEvents sa2 As Label
+    Friend WithEvents sa4 As Label
+    Friend WithEvents sa3 As Label
+    Friend WithEvents sb4 As Label
+    Friend WithEvents sb3 As Label
+    Friend WithEvents sb2 As Label
+    Friend WithEvents sb1 As Label
+    Friend WithEvents calculate As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    Dim a, b, c, d As Integer
+    Dim suma1, suma2, suma3, suma4 As Integer
+
+    Private Sub calculate_Click(sender As Object, e As EventArgs) Handles calculate.Click
+        a = Val(a1.Text)
+        b = Val(a2.Text)
+        c = Val(a3.Text)
+        d = Val(a4.Text)
+
+        suma1 = a
+        suma2 = b
+        suma3 = c
+        suma4 = d
+
+        a = Val(b1.Text)
+        b = Val(b2.Text)
+        c = Val(b3.Text)
+        d = Val(b4.Text)
+
+        sa1.Text = Val(suma1) + (a)
+        sa2.Text = Val(suma2) + (b)
+        sa3.Text = Val(suma3) + (c)
+        sa4.Text = Val(suma4) + (d)
+
+        sb1.Text = Val(suma1) - (a)
+        sb2.Text = Val(suma2) - (b)
+        sb3.Text = Val(suma3) - (c)
+        sb4.Text = Val(suma4) - (d)
+    End Sub
+End Class
